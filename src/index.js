b9ef46d63c8838f14b9a52c525636798d7e8b340
duongdung13 <dungdcth@gmail.com>
2023-06-02 09:42:35 +0700

    add express-handlebars and create layout web sample

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('express-handlebars').engine;
+
+const app = express();
+const port = 3000;
+
+//HTTP Logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('.hbs', hbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, "resources/views"));
+
+//Page home
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+//Page news
+app.get('/news', (req, res) => {
+  res.render('news', {
+    showTitle: true
+  });
+});
+
+//Page Not Load Layout
+app.get('/page-layout-false', (req, res, next) => {
+  res.render('pageLayoutFalse', {layout: false});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
