7435c418a8e453a90deab92a707516cf63f03e42
DungDC <dungdcth@gmail.com>
2025-08-05 16:24:57 +0700

    Nodejs with expressjs

--- nodejs/non-blocking.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-console.log('1. Bắt đầu đọc file');
-
-fs.readFile('test.txt', 'utf8', (err, data) => {
-  console.log('3. Nội dung file:', data);
-});
-
-console.log('2. Kết thúc đoạn code');
